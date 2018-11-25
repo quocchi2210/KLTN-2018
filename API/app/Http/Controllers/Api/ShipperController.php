e6669316beb19d9dabe7f993e99b59ce0d0ed965
quocchi2210 <quocchi2210@gmail.com>
2018-11-25 16:42:28 +0700

    feat(code): controller shipper

--- API/app/Http/Controllers/Api/ShipperController.php
+++ API/app/Http/Controllers/Api/ShipperController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use DB;
 use Log;
-
+use Config;
 class ShipperController extends Controller
 {
      /**
@@ -14,12 +14,6 @@ class ShipperController extends Controller
      *   path="/api/shipper/showOrder",
      *     tags={"Shipper"},
      *   summary="Show Profile",
-     *   @SWG\Parameter(
-     *     name="name_store",
-     *     in="query",
-     *     description="Your Name Store",
-     *     type="string",
-     *   ),
      *   @SWG\Response(
      *     response=200,
      *     description="A list with products"
@@ -33,27 +27,47 @@ class ShipperController extends Controller
      */
     public function showOrder(Request $request){
 
-    	$idShipper = 1;
-
 		if ($request->isMethod('post')) {
 
-			//$users = DB::statement("SELECT * FROM orders WHERE idShipper=?",[$idShipper]);
+			$idUser = $request->idUser;
+			//$idUser = 2;
+	    	$result_shipper = DB::table('shippers')->select('idShipper')->where('idUser', $idUser)->get();
+	    	if($result_shipper->count() > 0){
 
-			$users = DB::select("SELECT * FROM orders WHERE idShipper = ?",
- 				[$idShipper]);
+		    // Log::debug('idUser'. print_r($idUser,1));
+		    // Log::debug('idShipper'. print_r($result_shipper[0]['idShipper'],1));
 
-			if($users){
-				return response()->json([
-					'error' => false,
-					'data' => $users,
-					'errors' => null,
-				], 200);
+		    	$idShipper = $result_shipper[0]->idShipper;
+				// $users = DB::statement("SELECT * FROM orders WHERE idShipper=?",[$idShipper]);
+
+				// $users = DB::select("SELECT * FROM orders WHERE idShipper = ?",[$idShipper]);
+
+				$users = DB::table('orders')
+				->where('idShipper', $idShipper)
+				->where('idOrderStatus', Config::get('constants.status_type.confirm'))
+				->where_or('idOrderStatus', Config::get('constants.status_type.pickup'))
+				->where_or('idOrderStatus', Config::get('constants.status_type.delivery'))
+				->get();
+
+				if($users){
+					return response()->json([
+						'error' => false,
+						'data' => $users,
+						'errors' => null,
+					], 200);
+				}else{
+					return response()->json([
+	                         'error' => true,
+	                         'data' => null,
+	                         'errors' => null,
+	                    ], 400);
+				}
 			}else{
 				return response()->json([
-                         'error' => true,
-                         'data' => $users,
-                         'errors' => null,
-                    ], 400);
+                     'error' => true,
+                     'data' => null,
+                     'errors' => null,
+                ], 400);
 			}
 		}
     }
@@ -62,12 +76,6 @@ class ShipperController extends Controller
      *   path="/api/shipper/showHistory",
      *     tags={"Shipper"},
      *   summary="Show Profile",
-     *   @SWG\Parameter(
-     *     name="name_store",
-     *     in="query",
-     *     description="Your Name Store",
-     *     type="string",
-     *   ),
      *   @SWG\Response(
      *     response=200,
      *     description="A list with products"
@@ -83,20 +91,38 @@ class ShipperController extends Controller
 
 		if ($request->isMethod('post')) {
 
-			$users = DB::table('orders')->select('*')->whereRaw('idShipper', [1])->get();
+			$idUser = $request->idUser;
+			//$idUser =2;
+	    	$result_shipper = DB::table('shippers')->select('idShipper')->where('idUser', $idUser)->get();
+	 
+	    	if($result_shipper->count() > 0){
 
-			if($users){
-				return response()->json([
-					'error' => false,
-					'data' => $users,
-					'errors' => null,
-				], 200);
+		    	$idShipper = $result_shipper[0]->idShipper;
+
+				$result_order = DB::table('orders')
+				->where('idShipper', $idShipper)
+				->where('idOrderStatus', Config::get('constants.status_type.done'))
+				->get();
+
+				if($result_order->count() > 0){
+					return response()->json([
+						'error' => false,
+						'data' => $result_order,
+						'errors' => null,
+					], 200);
+				}else{
+					return response()->json([
+	                         'error' => true,
+	                         'data' => null,
+	                         'errors' => null,
+	                    ], 400);
+				}
 			}else{
 				return response()->json([
-                         'error' => true,
-                         'data' => $users,
-                         'errors' => null,
-                    ], 400);
+                     'error' => true,
+                     'data' => null,
+                     'errors' => null,
+                ], 400);
 			}
 		}
     }
@@ -105,12 +131,6 @@ class ShipperController extends Controller
      *   path="/api/shipper/updateStatus",
      *     tags={"Shipper"},
      *   summary="Show Profile",
-     *   @SWG\Parameter(
-     *     name="name_store",
-     *     in="query",
-     *     description="Your Name Store",
-     *     type="string",
-     *   ),
      *   @SWG\Response(
      *     response=200,
      *     description="A list with products"
@@ -125,21 +145,43 @@ class ShipperController extends Controller
     public function updateStatus(Request $request){
 
 		if ($request->isMethod('post')) {
+			$status_order = $request->get('status_order');
+			$idUser = $request->idUser;
+			// $idUser =2;
+			// $status_order=4;
+	    	$result_shipper = DB::table('shippers')->select('idShipper')->where('idUser', $idUser)->get();
 
-			$users = DB::table('orders')->select('*')->where('idShipper = ?',1)->get();
+	    	if($result_shipper->count() > 0){
 
-			if($users){
-				return response()->json([
-					'error' => false,
-					'data' => $users,
-					'errors' => null,
-				], 200);
+		    	$idShipper = $result_shipper[0]->idShipper;
+
+				if($status_order<Config::get('constants.status_type.done')){
+					$status_order++;
+				}
+
+		    	$affected = DB::table('orders')
+	           	->where('idShipper', $idShipper)
+	            ->update(['idOrderStatus' => $idOrderStatus]);
+
+				if($affected){
+					return response()->json([
+						'error' => false,
+						'data' => $affected,
+						'errors' => null,
+					], 200);
+				}else{
+					return response()->json([
+	                         'error' => true,
+	                         'data' => null,
+	                         'errors' => null,
+	                    ], 400);
+				}
 			}else{
 				return response()->json([
-                         'error' => true,
-                         'data' => $users,
-                         'errors' => null,
-                    ], 400);
+                     'error' => true,
+                     'data' => null,
+                     'errors' => null,
+                ], 400);
 			}
 		}
     }
---
+++ API/config/constants.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'status_type' => [
+        'pending' => '1',
+        'confirm' => '2',
+        'pickup' => '3',
+        'delivery' => '4',
+        'done' => '5',
+    ]
+];
+?>
