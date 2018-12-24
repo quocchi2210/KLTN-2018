e4ac2588a633439c6ce7376e9cfb1b9263f4b4f1
quocchi2210 <quocchi2210@gmail.com>
2018-12-24 17:42:32 +0700

    test9

--- Shipper_App/app/src/main/java/com/example/quocchi/shipper_app/MainActivity.java
+++ Shipper_App/app/src/main/java/com/example/quocchi/shipper_app/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
@@ -76,6 +77,8 @@ public class MainActivity extends AppCompatActivity {
     private void mapped_gui(){
         edt_user = (EditText)findViewById(R.id.edit_text_user);
         edt_password = (EditText)findViewById(R.id.edit_text_password);
+
+        edt_password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
         btn_login = (Button)findViewById(R.id.btn_login);
         btn_register = (Button)findViewById(R.id.btn_register);
         btn_logout = (Button)findViewById(R.id.btn_logout);
